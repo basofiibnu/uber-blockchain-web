51634765bfc959fd6d478b8b551dbd9d13fa49c0
basofiibnu <>
2022-03-22 18:59:01 +0700

    added input for dropoff and pickup

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import LocationSelector from '../components/LocationSelector';
 import Map from '../components/Map';
 import Navbar from '../components/Navbar';
 
@@ -5,8 +6,8 @@ const style = {
   wrapper: `h-screen w-screen flex flex-col`,
   main: `h-full w-screen flex-1 z-10`,
   mapContainer: `flex-1 w-full h-full`,
-  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end`,
-  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[500px] bg-white rounded-lg flex flex-col overflow-scroll`,
 };
 
 export default function Home() {
@@ -17,7 +18,9 @@ export default function Home() {
         <Map />
       </div>
       <div className={style.rideRequestContainer}>
-        <div className={style.rideRequest}></div>
+        <div className={style.rideRequest}>
+          <LocationSelector />
+        </div>
       </div>
     </div>
   );
